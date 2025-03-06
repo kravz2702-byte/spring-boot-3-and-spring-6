0488443bc07fe78839b0275cace1e5cc745d272f
user <kravz2702@gmail.com>
2025-03-06 23:47:01 +0300

    started 3rd chapter about OAS

---
+++ chapter3OAS/src/main/java/com/boot/chapter3oas/Chapter3OasApplication.java
@@ -0,0 +1,13 @@
+package com.boot.chapter3oas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter3OasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter3OasApplication.class, args);
+    }
+
+}
---
+++ chapter3OAS/src/test/java/com/boot/chapter3oas/Chapter3OasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.chapter3oas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter3OasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
